834146389afa68c6e0379730ca24bd403521e407
Patrick <prm5983@rit.edu>
2021-09-05 18:10:35 -0400

    Attempted to make a skid animation for moving, got fed up with lack of curve editor

--- PlayerControl.gd
+++ PlayerControl.gd
@@ -1,15 +1,24 @@
-extends CSGBox
+extends Spatial
 
 
 ## Variables ##
-#
+export var moveCooldown := 0.1
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var cooldownTimer := 0;
+var onCooldown := false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if !onCooldown:
+		if Input.is_action_just_pressed("ui_left"):
+			translation.x -= 2
+		elif Input.is_action_just_pressed("ui_right"):
+			translation.x += 2
+		translation.x = clamp(translation.x, -2, 2)
+	
+	elif cooldownTimer >= moveCooldown:
+		cooldownTimer = 0;
+		onCooldown = false;
+	else:
+		cooldownTimer += delta;
